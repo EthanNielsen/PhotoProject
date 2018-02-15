a780663565167ae50a3df23594a4219504e41263
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-02-15 11:57:46 -0700

    I had to set the rightPixel and leftPixel to the leftPixel and rightPixel and set the color their supposed to be.

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -247,16 +247,18 @@ public void glitchArt()
 	     {
 	    	 for (int col = 0; col < smallWidth / 2; col++)
 	    	 {
+	 	     leftPixel = smallPixel[row1][col];
+		     rightPixel = smallPixel[row1][smallWidth - 1 - col];
+		        
 	    		 redPixel = smallPixel[row1][col].getRed();
 	    		 greenPixel = smallPixel[row1][col].getGreen();
 	    		 
-	 	     for (smallPixel[row1][col].redPixel)
-	 	     {
-	 	    	 
-	 	     }
+	 	    
 		     rightPixel = smallPixel[row1][smallWidth - 1 - col];
-		     rightPixel.setColor(leftPixel.getColor());
-		     
+		     rightPixel.setGreen(leftPixel.getGreen());
+		     rightPixel.setRed(leftPixel.getRed());
+		     leftPixel.setGreen(rightPixel.getGreen());
+		     leftPixel.setRed(rightPixel.getRed());
 		     
 	    	 }
 	    }
